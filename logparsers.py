02960c7c6967a7a11da316428efe3d513b207b07
Adam  Chandler <alc28@cornell.edu>
2013-07-03 14:41:53 -0400

    pulled voyager search parser into gen module

--- logparsers.py
+++ logparsers.py
@@ -23,3 +23,21 @@ def geturlsearches(file):
 						allsearches.append(thesearch)
 
 	return allsearches
+
+
+def getvoyagersearches(file):
+	allsearches = []
+	with open(file) as f:
+	    lines = f.readlines()
+	
+	for i in range (3,len(lines)-1):
+		for tup in range (0, int(lines[i].split()[1])):
+			o = urlparse(lines[i].split()[0])
+			q = o.query.split('&')
+			for val in q:
+				if 'Search_Arg' in val:
+					thesearch = val.split('=')[1].replace('+', ' ')
+					print thesearch
+					allsearches.append(urllib.unquote(thesearch).strip())
+	
+	return allsearches
