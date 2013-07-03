4d95857eb8f2e2b8e4165e241f9043052b6ac646
Adam  Chandler <alc28@cornell.edu>
2013-07-03 12:17:05 -0400

    created module for libguides

--- bb.py
+++ bb.py
@@ -2,6 +2,10 @@
 # best bet 
 
 import sys
+import os
+import pandas as pd
+
+os.linesep = '\n'
 #import nltk
 
 if 'voyager' in sys.argv[1]:
@@ -11,9 +15,59 @@ if 'voyager' in sys.argv[1]:
 	print len(v)
 
 if 'libguides' in sys.argv[1]:
-	print 'choice = libguides'
+	inputpathfile = 'input/' + sys.argv[1] + '/' + sys.argv[2]
+	print inputpathfile
 	import mlibguides
-	v = mlibguides.getsearches(sys.argv[2])	
-	print len(v)
-	#for line in v:
-		#print line
+	v = mlibguides.getsearches(inputpathfile)
+	v.sort()
+	weillseries = pd.Series(v)
+	print weillseries.count()
+	outputpathfile = 'output/' + sys.argv[1] + '/' + sys.argv[2] + '-out.csv'
+	print outputpathfile
+	weillseries.value_counts().to_csv(outputpathfile)
+	
+
+
+#	v.sort()
+#	for line in v:
+#		f.write(line + os.linesep)
+#		
+#	vunique = list(set(v))
+#	vunique.sort()
+#	for line in vunique:
+#		u.write(line + os.linesep)
+#
+#	cfilepath = 'output/libguides/libguides-unique-search-terms.tsv'
+#	if os.path.exists(cfilepath) is False:
+#		print 'gr cluster file is not available'
+#	else:
+#		print 'gr cluster file is available'
+#		cdict = {}
+#		with open(cfilepath) as cf:
+#			lines = cf.readlines()
+#			for i in range (1,len(lines)):
+#				cols = lines[i].split("\t")
+#				cdict[cols[0]] = cols[1]
+#		cleanlist = []
+#		for line in v:
+#			if line in cdict.keys():
+#				cleanlist.append(cdict[line].rstrip())
+#		cleanlist.sort()
+#		from collections import Counter
+#		termcount = Counter()
+#		for cleanline in cleanlist:
+#			cleaned.write(cleanline + os.linesep)
+#			termcount[cleanline] += 1
+#		
+#		print termcount.most_common(10)
+#		cleancount.write('clustered term' + "\t" + 'count' + os.linesep)
+#		for key, val in termcount.iteritems():
+#			#cleancount.write(key + "\t" + str(val))
+#			writeout = key + "\t" + str(val)
+#			cleancount.write(writeout + os.linesep)
+			
+		
+		
+
+		
+
--- mlibguides.py
+++ mlibguides.py
@@ -4,23 +4,25 @@ import urllib
 from urlparse import urlparse
 
 def getsearches(file):
+	import re
 	allsearches = []
-	counter = 0
 	with open(file) as f:
-	    lines = f.readlines()
+	  lines = f.readlines()
 	
 	for i in range (3,len(lines)-1):
-		counter = counter + 1
+		print i
 		for tup in range (0, int(lines[i].split()[1])):
 			o = urlparse(lines[i].split()[0])
 			q = o.query.split('&')
 			for val in q:
-				if 'q' in val:
-					thesearch = val.split('=')[1].replace('+', ' ')
-					cols = lines[i].split('\t')
-					print cols[1]
-					allsearches.append(urllib.unquote(thesearch).strip())
+				if 'q=' in val:
+					thesearch = val.split('=')[1]
+					if len(thesearch) > 1:
+						thesearch = urllib.unquote(thesearch).strip().lower().translate(None, '"')
+						thesearch = thesearch.replace('%22', ' ')
+						thesearch = thesearch.replace("\t", ' ')
+						thesearch = thesearch.replace("+", ' ')
+						allsearches.append(thesearch)
 
-	
 	return allsearches
 
